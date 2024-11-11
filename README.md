e91b9daffba6c2eebb29fd7d71c8fcf91fa8a66b
김보미 <kimbomi2172@naver.com>
2024-11-11 14:14:00 +0900

    ✨ feat: 사용자 위치 띄우기

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # rn-playground
+
+https://reactnative.dev/docs/components-and-apis
+
+## react-native 규칙
+
+```js
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>hello!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 28,
+  },
+})
+```
+
+1. react-native는 웹사이트가 아님. div 못 씀! -> View를 쓰자.
+2. 모든 text는 Text 컴포넌트에 쓰자.
+3. View에 style이 있고, 원하는대로 styles 가능. 이건 웹의 react.js에서 할 수 있는 것과 비슷함. (but 전부는 아님.. 1px green solid 같은..)
+4. StyleSheet.create는 object를 생성하는 데 사용함. (자동 완성 기능 제공, 거대한 style 방지)
+5. status bar(상태표시줄)는 다음 강의에 (시계, 인터넷, 배터리 등을 표시, 운영체제와 소통하는 component라는 증거)
--- WeatherApp/App.js
+++ WeatherApp/App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react'
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window')
 
 export default function App() {
+  const [location, setLocation] = useState()
+  const [ok, setOk] = useState(true)
+
+  const [city, setCity] = useState()
+
+  const ask = async () => {
+    // 앱 사용하는 중에만 위치 허용
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+    if (!granted) setOk(false)
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    )
+    setCity(location[0].city)
+  }
+
+  useEffect(() => {
+    ask()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>테스트!!!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -13,8 +64,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#4facfe',
+  },
+  city: {
+    flex: 1.2,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    color: 'black',
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 50,
+    marginTop: -30,
   },
 })
